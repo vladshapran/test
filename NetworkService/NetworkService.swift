dba59a0f6f069f51c5e8e2bfd998f07fb07ecfbc
vladshapran <56024707+vladshapran@users.noreply.github.com>
2021-02-08 21:26:40 +0200

    Create NetworkService.swift

---
+++ NetworkService/NetworkService.swift
@@ -0,0 +1,35 @@
+import Foundation 
+
+
+typealias MovieClosure = (Result) -> (Void)
+
+protocol MovieAPI {
+  func fetchMovie(completion: MovieClosure) -> (Void)
+}
+
+class NetworkService { 
+  static let shared: NetworkService = NetworkService()
+  private init() {}
+}
+
+extension NetworkService: MovieAPI {
+
+  func fetchMovie(completion: MovieClosure) {
+    let urlString  = "https://api.themoviedb.org/3/movie/popular?api_key=505cc219eeba7ba5c8c15a6d0a5b3779&language=en-US&page=1"
+    guard url = URL(string: urlString) else { return }
+    
+    URLSession.shared.dataTask(with: url) { data, _, error in 
+      if let error = error {
+        print("\(error)")
+        return
+      }
+      
+      do {
+        let obj = try JSONDecoder().decode(Result.self, from: data!)
+        completion(Result)
+      } catch {
+        print("\(error)")
+      }
+    }.resume()
+  }
+}
