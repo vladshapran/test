0ab2b1efde9bc12d40c8b609263ff8d0db1beaa0
vladshapran <56024707+vladshapran@users.noreply.github.com>
2021-02-08 21:26:21 +0200

    Update MoviePresenter.swift

--- MoviesModule/MoviePresenter.swift
+++ MoviesModule/MoviePresenter.swift
@@ -1,8 +1,43 @@
 
-protocol MoviePresentation {
 
+
+protocol MoviePresentation {
+  func viewDidLoad() -> (Void)
 }
 
 class MoviePresenter {
+  weak var view: MovieView? 
+  var interactor: MovieUseCase?
+  var router: MovieRouting? 
+  
+  init(view: MovieView, interactor: MovieUseCase, router: MovieRouting) {
+    self.view = view
+    self.interactor = interactor
+    self.router = router
+  }
+    
+}
 
+extension MoviePresenter: MoviePresentation {
+  func viewDidLoad(){
+    self.interactor.getMovie(completion: { (result) in
+        let movieList = result.
+    })
+  }
+}
+
+struct MovieItemModel {
+    let backdrop_path: String?
+    let original_title: String?
+    let overview: String?
+    let release_date: String?
+    let genre_ids: [Int?]
+  
+  init(using movieModel: Movie){
+    self.backdrop_path = backdrop_path
+    self.original_title = original_title
+    self.overview = overview
+    self.release_date = release_date
+    self.genre_ids = genre_ids
+  }
 }
