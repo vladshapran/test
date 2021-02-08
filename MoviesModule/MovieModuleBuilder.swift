b519252d56af2db57533bb49a22789bfe218b988
vladshapran <56024707+vladshapran@users.noreply.github.com>
2021-02-08 21:26:12 +0200

    Update MovieModuleBuilder.swift

--- MoviesModule/MovieModuleBuilder.swift
+++ MoviesModule/MovieModuleBuilder.swift
@@ -4,7 +4,11 @@ class MovieModuleBuilder {
   func build() -> UIViewController {
        
        let view = MovieVC()
-       
+       let presenter = MoviePresenter(interactor: interactor, router: router)
+       let router = MovieRouter(view: view)
+       let interactor = MovieInteractor(service: NetworkService.shared)
+       view.presenter = presenter
+    
        return view
    }
 }
