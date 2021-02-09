505e8497a166e0892f5a28caa304471d7e8e100c
vladshapran <56024707+vladshapran@users.noreply.github.com>
2021-02-09 18:01:11 +0200

    Update TabBarModuleBuilder.swift

--- Tabbar/TabBarModuleBuilder.swift
+++ Tabbar/TabBarModuleBuilder.swift
@@ -1,8 +1,9 @@
 import UIKit
 
 class TabBarModuleBuilder {
-  static func build() -> UITabBarController {
-       
-       fatalError()
+  static func build(usingSubmodules submodules: TabBarRouter.Submodules) -> UITabBarController {
+    let tabs  = TabBarRouter.Submodules(usingSubmodules: submodules)
+    let tabBarController = TabBarController(tabs: tabs)  
+    return tabBarController
    }
 }
