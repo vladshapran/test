5664d07fa2288a5707176b03da9bf0cad2638b69
vladshapran <56024707+vladshapran@users.noreply.github.com>
2021-02-09 12:22:00 +0200

    Update FavouritesVC.swift

--- FavouritesModule/FavouritesVC.swift
+++ FavouritesModule/FavouritesVC.swift
@@ -2,4 +2,26 @@ import UIKit
 
 class FavouritesVC: UIViewController {
   
+  let tableView = UITableView()
+  let movieCell = "CustomMovieTableViewCell
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    createTableView()
+  }
+  
+  func createTableView() {
+        view.addSubview(tableView)
+        tableView.register(UINib.init(nibName: movieCell, bundle: nil), forCellReuseIdentifier: movieCell)
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+      
+    }
+  
 }
