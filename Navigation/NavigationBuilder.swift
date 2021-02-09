e88162436c6f3a5c4043bc5497eca11165f010c4
vladshapran <56024707+vladshapran@users.noreply.github.com>
2021-02-09 11:59:50 +0200

    Update NavigationBuilder.swift

--- Navigation/NavigationBuilder.swift
+++ Navigation/NavigationBuilder.swift
@@ -1,5 +1,10 @@
 import UIKit
 
+typealias NavigationFactory = (UIViewController) -> (UINavigationController)
+
 class NavigationBuilder {
-  
+  static func build(rootView: UIViewController) -> UINavigationController {
+    let navigationController = UINavigationController(rootViewController: rootView)
+    return navigationController
+  }
 }
