4dcb2deb0bb680304ce8ea26391c0e4f01684500
vladshapran <56024707+vladshapran@users.noreply.github.com>
2021-02-09 18:56:06 +0200

    Update DetailsVC.swift

--- DetailsModule/DetailsVC.swift
+++ DetailsModule/DetailsVC.swift
@@ -2,13 +2,39 @@ import UIKit
 
 class DetailsVC: UIViewController {
 
-    @IBOutlet var name: UILabel!
-    @IBOutlet var genres: UILabel!
+    var descriptions = UILabel()
+    var releaseData = UILabel()
+    var imageMovie = UIImageView()
+    
+    var movie: MovieItemModel 
    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Detail Info"
-          
+        settingUpObjects()
+       
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+            self.title = movie?.original_title
+            descriptions.text = "Описание" + (movie?.overview ?? " ")
+            releaseData.text = "Дата релиза: " + (movie?.release_date ?? " ")
+        
+            let url = URL(string: "https://image.tmdb.org/t/p/w500/"  + (movie?.backdrop_path ?? " " )) ?? nil
+            if let data = try? Data(contentsOf: url!) {
+                image?.image = UIImage(data: data)
+         }
+    }
+    
+    func settingUpObjects() {
+        
+        
+        view.addSubview(descriptions)
+        view.addSubview(releaseData)
+        view.addSubview(imageMovie)
+        
+    }
+  
+        
+    
     
 }
