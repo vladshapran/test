4c9e068d25d694bc711471c263133ef8fa2f8519
vladshapran <56024707+vladshapran@users.noreply.github.com>
2021-02-09 19:25:41 +0200

    Update NetworkService.swift

--- NetworkService/NetworkService.swift
+++ NetworkService/NetworkService.swift
@@ -3,6 +3,8 @@ import Foundation
 
 typealias MovieClosure = (Result) -> (Void)
 
+// MARK: -Protocol.
+
 protocol MovieAPI {
   func fetchMovie(completion: MovieClosure) -> (Void)
 }
@@ -14,6 +16,8 @@ class NetworkService {
 
 extension NetworkService: MovieAPI {
 
+  // MARK: -Getting movies from the network.
+  
   func fetchMovie(completion: MovieClosure) {
     let urlString  = "https://api.themoviedb.org/3/movie/popular?api_key=505cc219eeba7ba5c8c15a6d0a5b3779&language=en-US&page=1"
     guard url = URL(string: urlString) else { return }
