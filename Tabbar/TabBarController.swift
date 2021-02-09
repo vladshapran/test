5ba041546e0b71d69ca86bc52eaf339f247793d4
vladshapran <56024707+vladshapran@users.noreply.github.com>
2021-02-09 16:59:20 +0200

    Create TabBarController.swift

---
+++ Tabbar/TabBarController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class TabBarController: UITabBarController {
+  init() {
+    super.init()
+  }
+  
+  required init?(coder aDecoder: NSDecoder) {
+    fatalError("Error")
+  }
+  
+  override func viewDidLoad(){
+    super.viewDidLoad
+  } 
+}
